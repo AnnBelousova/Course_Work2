c457e560ac6f4f7f762120710e3701c74306218c
Anna <anna051977@gmail.com>
2022-12-25 19:16:05 +0300

    second commit

--- src/EveryYearTask.java
+++ src/EveryYearTask.java
@@ -3,5 +3,16 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 public class EveryYearTask implements Frequency {
-
+    @Override
+    public Set<Task> getNextDate(LocalDate localDate, Map<Integer, Task> taskList) {
+        Set<Task> list = new HashSet<>();
+        for (Map.Entry<Integer, Task> entry : taskList.entrySet()) {
+            LocalDateTime taskDateEveryEar = LocalDateTime.of(entry.getValue().getLocalDate().getYear() + 1,
+                    entry.getValue().getLocalDate().getMonth(), entry.getValue().getLocalDate().getDayOfMonth(),
+                    entry.getValue().getLocalDate().getHour(), entry.getValue().getLocalDate().getMinute());
+            Task newTaskEveryDay = new Task(entry.getValue().getTaskName(), entry.getValue().getDescription(), entry.getValue().getTaskType(), taskDateEveryEar, entry.getValue().getFrequency(), entry.getValue().getActive());
+            list.add(newTaskEveryDay);
+        }
+        return list;
+    }
 }
--- src/Validation.java
+++ src/Validation.java
@@ -1,3 +1,18 @@
 public class Validation {
+    String field;
 
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public void throwExceptionStringField(String str) {
+        if (str == "" || str.isEmpty() || str.isBlank()) {
+            throw new IllegalArgumentException("Это поле обязательно для заполнения или некорректно заполнено");
+        } else {
+        }
+    }
 }
